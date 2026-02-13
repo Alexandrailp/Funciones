49d52873b678683e8bfd368b816b31c28560037a
Alexandra Lara <alexandrailp.r@gmail.com>
2026-02-13 12:42:12 -0300

    segundo ejercicio finalizado

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,11 @@
-const cuerpo = document.querySelector(".cuerpo");
+const ele = document.getElementById("ele1");
 
-function pintar(color){
-    cuerpo.style.backgroundColor = color;
+function pintar(elemento, color = "green") {
+    elemento.style.backgroundColor = color;
 }
 
+pintar(ele);
+
+ele.addEventListener("click", function(event) {
+    pintar(event.target, "yellow");
+});
